e2d13248417add0542233d3d462ae34a6317ad24
Osster <_oster_@mail.ru>
2018-08-16 19:36:39 +0300

    Favicon Updated

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -22,11 +22,13 @@
     <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png">
     <link rel="manifest" href="/icons/site.webmanifest">
-    <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#d82f2f">
+    <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5">
     <link rel="shortcut icon" href="/icons/favicon.ico">
-    <meta name="msapplication-TileColor" content="#00aba9">
+    <meta name="msapplication-TileColor" content="#da532c">
     <meta name="msapplication-config" content="/icons/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
+
+
 
     @yield("PAGE_STYLES")
 </head>
